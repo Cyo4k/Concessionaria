0daf3671a783885cb7bcd5893edcef06c59aa0ac
João Brasileiro <93006521+Cyo4k@users.noreply.github.com>
2024-09-11 14:52:23 +0000

    Integrando o sistema de login e validação, cadastramento de funcionários.

--- carros.js
+++ carros.js
@@ -9,12 +9,14 @@ Parse.initialize("ziF0QzwENpNeDImCmQ6BicKnwIB2dYCO035Em9pL", "9Pz6oMi7vMZOZNB7i6
 document.getElementById("vehicleForm").addEventListener("submit", async function(event) {
   event.preventDefault();
 
+  const marca = document.getElementById("marca").value;
   const model = document.getElementById("model").value;
   const cor = document.getElementById("cor").value;
   const ano = parseInt(document.getElementById("ano").value);
   const price = parseFloat(document.getElementById("price").value);
 
   const carro = new Parse.Object("Carro");
+  carro.set("marca", marca);
   carro.set("model", model);
   carro.set("cor", cor);
   carro.set("ano", ano);
